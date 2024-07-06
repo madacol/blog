6ea2c2680a0058428e6df24ebd833d5bdcbb89bd
Marco D'Agostini <madacol10@gmail.com>
2024-07-07 01:54:27 +0200

    inputFile -> file

--- bin/posts.sh
+++ bin/posts.sh
@@ -4,22 +4,22 @@
 
 runFolder=$(pwd)
 
-for inputFile in "$@";
+for file in "$@";
 do
 
   echo "Generating \"$outputFile\""
 
   # replace file extension in internal links (e.g. [link](file.md) -> [link](file.html))
-  sed -E 's#\[(.*?)\]\((.+?)\.md\)#[\1](\2.html)#g' "$inputFile" > /dev/shm/parsedContent.md
+  sed -E 's#\[(.*?)\]\((.+?)\.md\)#[\1](\2.html)#g' "$file" > /dev/shm/parsedContent.md
 
-  outputFile=${inputFile%.*}.html
+  outputFile=${file%.*}.html
 
   pandoc <(./bin/parseImport.sh /dev/shm/parsedContent.md) \
     -o "$outputFile" \
     -f markdown \
     --pdf-engine wkhtmltopdf \
     --pdf-engine-opt=--enable-local-file-access \
-    --resource-path=$(dirname "$inputFile") \
+    --resource-path=$(dirname "$file") \
     --css $runFolder/content/style.css \
     --self-contained \
     --metadata pagetitle="`sed -n 's/^# //p' "$inputFile" | head -n 1`" \
