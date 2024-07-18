c9a6c81a9ca8dfcc6124e3529f7c32ac6ecfc8d5
Marco D'Agostini <madacol10@gmail.com>
2024-07-18 16:43:51 +0200

    allow links to be followed by the character `\` and still be ignored

--- bin/parseImport.sh
+++ bin/parseImport.sh
@@ -6,5 +6,4 @@
 # Inspired by https://stackoverflow.com/a/18517316/3163120
 
 perl -ne 's#^!\[\[(.+?)\]\]#`'$0' "$1.md"`#e;print' "$@" | \
-sed -E 's#(^|\s|[^\]\()(https?://[^ )]+)#\1<\2>#g' "$@"
-# sed command to parse raw URLs in markdown files and then wrap them in `<...>` tags
+sed -E 's#(^|\s|[^\]\()(https?://[^ )\\]+)#\1<\2>#g' "$@" # sed command to parse raw URLs in markdown files and then wrap them in `<...>` tags
