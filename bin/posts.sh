0183c097dbb80e4f9fbf032425e29253ff327d13
Marco D'Agostini <madacol10@gmail.com>
2024-07-19 11:30:13 +0200

    Fix: incorrect logging, files were shifted, and last file was ignored

--- bin/posts.sh
+++ bin/posts.sh
@@ -7,12 +7,12 @@ runFolder=$(pwd)
 for file in "$@";
 do
 
+  outputFile=${file%.*}.html
   echo "Generating \"$outputFile\""
 
   # replace file extension in internal links (e.g. [link](file.md) -> [link](file.html))
   sed -E 's#\[(.*?)\]\((.+?)\.md\)#[\1](\2.html)#g' "$file" > /dev/shm/parsedContent.md
 
-  outputFile=${file%.*}.html
   title=$(sed -n 's/^# //p' "$file" | head -n 1)
 
   pandoc <(./bin/parseImport.sh /dev/shm/parsedContent.md) \
