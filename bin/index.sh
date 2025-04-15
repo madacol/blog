c9ce6be482ccc3f799fed7e1bc328dd7474531e3
Marco D'Agostini <madacol10@gmail.com>
2025-04-15 09:46:23 +0100

    It looked too small on mobile

--- bin/index.sh
+++ bin/index.sh
@@ -10,6 +10,7 @@ htmlDoc="<!DOCTYPE html>
 <html>
 <head>
   <meta charset=\"utf-8\">
+  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=yes\" />
   <title>Madacol's Blog</title>
   <link rel=\"stylesheet\" href=\"style.css\">
   <style>
