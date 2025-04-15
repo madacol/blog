f63f958b864c42dd3bf1f4cf5fb3143f216e69a0
Marco D'Agostini <madacol10@gmail.com>
2025-04-15 10:16:08 +0100

    Changed build from github action to a local build (sh script)

---
+++ bin/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+./bin/posts.sh content/*.md
+./bin/index.sh content/*.md
+
+touch package.json # Stupid gh-pages fails if there's no package.json ¬¬
+
+echo "Deploying..."
+npx gh-pages -d './content' --nojekyll
